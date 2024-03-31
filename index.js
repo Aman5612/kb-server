20a67d72d8391c618a58d23f60c254c13323da22
Aman Tiwari <amankumartiwari392@gmail.com>
2024-04-01 03:35:20 +0530

    port upadte

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 app.use(cors());
 
 app.use(bodyParser.json());
